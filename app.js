c0fb25647db4ea72d179b02b027dd55a3f849358
Sonia Meller <mellersonia@gmail.com>
2019-06-27 15:14:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+// 1 -- $ npm run dev
+// 2 -- $ npm start --> Dont run. It is just for going online
+
+// save project on github
+// 1 -- git init
+// 2 -- git commit -m "first commit"
+// 3 -- git remote add origin https://github.com/soniameller/lab-express-ironstory.git
+// 4 -- git push -u origin master
+
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+
+
+mongoose
+  .connect('mongodb://localhost/lab-express-ironstory', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+      
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+
+const index = require('./routes/index');
+app.use('/', index);
+
+
+module.exports = app;
